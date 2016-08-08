35b9cb3fd2340f91c934c11df56989870bf36ac7
Paxi <dev@paxii.de>
2016-08-08 21:55:01 +0200

    [TASK] Add speed slider

--- src/main/java/de/paxii/clarinet/module/external/ModuleFreecam.java
+++ src/main/java/de/paxii/clarinet/module/external/ModuleFreecam.java
@@ -8,6 +8,7 @@ import de.paxii.clarinet.event.events.player.PlayerUpdateWalkingEvent;
 import de.paxii.clarinet.event.events.player.PreMotionUpdateEvent;
 import de.paxii.clarinet.module.Module;
 import de.paxii.clarinet.module.ModuleCategory;
+import de.paxii.clarinet.util.module.settings.ValueBase;
 import lombok.Value;
 import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.util.math.BlockPos;
@@ -25,16 +26,16 @@ public class ModuleFreecam extends Module {
 	public ModuleFreecam() {
 		super("Freecam", ModuleCategory.MOVEMENT);
 
-		this.setVersion("1.1");
+		this.setVersion("1.2");
 		this.setBuildVersion(16005);
+
+		this.getModuleValues().put("speed", new ValueBase("Freecam Speed", 10.0F, 1.0F, 20.0F, "Speed"));
 
 		this.random = new Random();
 	}
 
 	@Override
 	public void onEnable() {
-		Wrapper.getEventManager().register(this);
-
 		this.entityID = -random.nextInt(99);
 
 		EntityOtherPlayerMP decoyPlayer = new EntityOtherPlayerMP(Wrapper.getWorld(), Wrapper.getPlayer().getGameProfile());
@@ -49,11 +50,13 @@ public class ModuleFreecam extends Module {
 				Wrapper.getPlayer().rotationYawHead,
 				Wrapper.getPlayer().rotationPitch
 		);
+
+		Wrapper.getEventManager().register(this);
 	}
 
 	@EventHandler
 	public void onIngameTick(IngameTickEvent event) {
-		float flightSpeed = 2.5F;
+		float flightSpeed = this.getValueBase("speed").getValue() / 4;
 
 		Wrapper.getPlayer().jumpMovementFactor = flightSpeed;
 
@@ -76,8 +79,8 @@ public class ModuleFreecam extends Module {
 	@EventHandler
 	public void onEntityMove(EntityMoveEvent moveEvent) {
 		if (moveEvent.getEntity() == Wrapper.getPlayer()) {
-			moveEvent.setX(moveEvent.getX() * 10D);
-			moveEvent.setZ(moveEvent.getZ() * 10D);
+			moveEvent.setX(moveEvent.getX() * this.getValueBase("speed").getValue());
+			moveEvent.setZ(moveEvent.getZ() * this.getValueBase("speed").getValue());
 			moveEvent.setNoClip(true);
 		}
 	}
@@ -89,6 +92,8 @@ public class ModuleFreecam extends Module {
 
 	@Override
 	public void onDisable() {
+		Wrapper.getEventManager().unregister(this);
+
 		Wrapper.getPlayer().setPosition(
 				this.savedLocation.getBlockPos().getX(),
 				this.savedLocation.getBlockPos().getY(),
@@ -98,8 +103,6 @@ public class ModuleFreecam extends Module {
 		Wrapper.getPlayer().rotationYaw = this.savedLocation.getRotationYaw();
 		Wrapper.getPlayer().rotationYawHead = this.savedLocation.getRotationYawHead();
 		Wrapper.getWorld().removeEntityFromWorld(this.entityID);
-
-		Wrapper.getEventManager().unregister(this);
 	}
 
 	@Value
